ef7866e57537125dbf66190131cfcb98c6caa056
Sanjula Dilhara Haputhanthrige <dilhara2002sanju@gmail.com>
2026-02-21 17:15:12 +0530

    Write to file

--- app.js
+++ app.js
@@ -5,3 +5,10 @@ fs.readFile('file.txt', 'utf8', function (err, data) {
  if (err) throw err;
  console.log(data);
 });
+
+//Write to file
+const fs = require('fs');
+fs.writeFile('file.txt', 'Hello World!', function (err) {
+ if (err) throw err;
+ console.log('File saved!');
+});
