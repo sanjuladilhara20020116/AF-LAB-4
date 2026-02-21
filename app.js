baba01803960ae11604435b4ffe9442a7382e9f5
Sanjula Dilhara Haputhanthrige <dilhara2002sanju@gmail.com>
2026-02-21 17:20:34 +0530

    Promises

--- app.js
+++ app.js
@@ -39,3 +39,17 @@ https.get('https://jsonplaceholder.typicode.com/posts/1', (resp) => {
 //Using a module
 const myModule = require('./my-module.js');
 console.log(myModule.myFunction());
+
+//Promises
+const myPromise = new Promise((resolve, reject) => {
+ if (condition) {
+ resolve('Success!');
+ } else {
+ reject('Failure!');
+ }
+});
+myPromise.then((result) => {
+ console.log(result);
+}).catch((error) => {
+ console.log(error);
+});
