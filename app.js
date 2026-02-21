b9d213e5ff2aa201757dbe53ddea75bc5b7fa370
Sanjula Dilhara Haputhanthrige <dilhara2002sanju@gmail.com>
2026-02-21 17:16:42 +0530

    Creating a web server:

--- app.js
+++ app.js
@@ -12,3 +12,12 @@ fs.writeFile('file.txt', 'Hello World!', function (err) {
  if (err) throw err;
  console.log('File saved!');
 });
+
+//Creating a web server
+
+const http = require('http');
+http.createServer(function (req, res) {
+ res.writeHead(200, {'Content-Type': 'text/html'});
+ res.write('Hello World!');
+ res.end();
+}).listen(8080);
